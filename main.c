9b07d96151eab57f2dec0ef37d13d2ef3f99f67a
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2018-12-01 19:28:50 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,27 +3,7 @@
 
 int main(int argc, char const *argv[])
 {
-    Node_AVL *root = NULL;
-
-    root = insert(root, 10);
-    root = insert(root, 20);
-    root = insert(root, 30);
-    root = insert(root, 40);
-    root = insert(root, 50);
-    root = insert(root, 25);
-
-    printf("Preorder traversal setelah insertion: ");
-    preorder(root);
-
-    root = delete_node(root, 10);
-    printf("\nPreorder traversal setelah penghapusan nilai 10: ");
-    preorder(root);
-
-    root = destroy_tree(root);
-    printf("\nPreorder traversal setelah Deletion: ");
-    preorder(root);
-
-    printf("\nRoot value: %d\n", root);
+    
 
     return 0;
 }
