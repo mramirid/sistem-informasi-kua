b4ae2ee9685b38459293997964fbb18fa284f96e
chilaact <32344674+chilaact@users.noreply.github.com>
2018-12-01 10:44:40 +0700

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,1 +1,20 @@
-//
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node
+{
+	struct node *next, *prev;
+	int nomor_antrian;
+	char casa[100], casi[100], alamat[100], tgl_nikah[100], lengkapdata[10];	
+};
+
+//================================================
+
+struct queue
+{
+	int count;
+	node *front;
+	node *rear;
+};
